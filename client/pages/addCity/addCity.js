c64d40f436ead03599ec4a3e4cfafc9a93494338
SimonYin <872224140@qq.com>
2018-04-28 02:10:05 +0800

    添加addCity页面

---
+++ client/pages/addCity/addCity.js
@@ -0,0 +1,63 @@
+//index.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+
+Page({
+    data: {
+        userInfo: {},
+        logged: false,
+        takeSession: false,
+        requestResult: '',
+        imgUrl:null,
+    },
+
+    // 上传图片接口
+    doUpload: function () {
+        var that = this
+
+        // 选择图片
+        wx.chooseImage({
+            count: 1,
+            sizeType: ['compressed'],
+            sourceType: ['album', 'camera'],
+            success: function(res){
+                util.showBusy('正在上传')
+                var filePath = res.tempFilePaths[0]
+
+                // 上传图片
+                wx.uploadFile({
+                    url: config.service.uploadUrl,
+                    filePath: filePath,
+                    name: 'file',
+
+                    success: function(res){
+                        util.showSuccess('上传图片成功')
+                        console.log(res)
+                        res = JSON.parse(res.data)
+                        console.log(res)
+                        that.setData({
+                            imgUrl: res.data.imgUrl
+                        })
+                    },
+
+                    fail: function(e) {
+                        util.showModel('上传图片失败')
+                    }
+                })
+
+            },
+            fail: function(e) {
+                console.error(e)
+            }
+        })
+    },
+
+    // 预览图片
+    previewImg: function () {
+        wx.previewImage({
+            current: this.data.imgUrl,
+            urls: [this.data.imgUrl]
+        })
+    },
+})
